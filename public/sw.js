cc8eb66da1d0c78acda07a0c89377f9a79e7edce
shinexlearning-cyber <shinexlearning@gmail.com>
2026-09-06 18:44:09 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'shinex-shell-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/','/manifest.webmanifest','/shinex-icon.svg'])));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).pathname.startsWith('/api')) return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((response) => response || caches.match('/'))));
+});

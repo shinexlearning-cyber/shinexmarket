37f771c5186d3a49af7cd20c18ebfaa2e0482de7
shinexlearning-cyber <shinexlearning@gmail.com>
2026-09-07 10:59:19 +0100

    Delete src directory

--- src/api/activity.js
+++
@@ -1,5 +0,0 @@
-import { api } from './client';
-
-export async function getActivity({ page = 1, limit = 20 } = {}) {
-  return api.get(`/activity?page=${page}&limit=${limit}`);
-}
--- src/api/contact.js
+++
@@ -1,9 +0,0 @@
-import { api } from './client';
-
-export async function sendContactMessage({ name, email, phone, subject, message }) {
-  return api.post('/contact', { name, email, phone, subject, message }, { auth: false });
-}
-export async function getContactInfo() {
-  const res = await api.get('/contact/info', ).catch(() => null);
-  return res?.data;
-}
